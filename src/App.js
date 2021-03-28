670c71a10408b07c722a1b9cad3dc1dc0176a2fb
michellebakels <michelle.bakels@gmail.com>
2021-03-28 18:46:22 -0400

    coding with bravery implementation

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {braveFigures} from "./utils";
+
+const generateFigure = (displayedFigure, setFigure) => {
+    const figureIndex = displayedFigure !== undefined ?
+        braveFigures.findIndex(figure => figure.name === displayedFigure.name) : 0
+
+    const newFigure = figureIndex === braveFigures.length - 1 ? braveFigures[0] : braveFigures[figureIndex + 1]
+
+    return setFigure(newFigure)
+}
 
 function App() {
+
+    const [figure, setFigure] = useState(undefined)
+
+    console.log({figure})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <h1>Inspirational Bravery</h1>
+        <h2>Explore the stories of brave women and men throughout history and around the world</h2>
+        {figure &&
+            <div className="container">
+                <div className="card">
+                    <section>
+                        <div id="image-item" className="item">
+                            <img src={figure?.image} alt={figure?.alt} className="card-img"/>
+                        </div>
+                        <div id="name" className="name">{figure?.name}</div>
+                        <p>
+                            {figure.role}<br/><br/>
+                            {figure.story}<br/>
+                        </p>
+                        <a href={figure.link} target="_blank" className="card-tag"><button className="learn-more">Learn More</button></a>
+                    </section>
+                </div>
+            </div>
+        }
+        <button onClick={() => generateFigure(figure, setFigure)} className="explore">EXPLORE</button>
+        <footer>Thank you to Nicholas Monacky for contributing to <a href="http://coding-with.web.app/" target="_blank">Coding with Bravery</a></footer>
+    </>
   );
 }
 
---
+++ src/utils.js
@@ -0,0 +1,82 @@
+export const braveFigures = [
+    {
+        name: 'Neil Armstrong and Buzz Aldrin',
+        role: 'Apollo 11 Astronauts',
+        story: 'Armstrong and Aldrin became the first humans ever to land on the moon. About six-and-a-half hours later, Armstrong became the first person to walk on the moon. As he took his first step, Armstrong famously said, "That\'s one small step for man, one giant leap for mankind."',
+        link: 'https://en.wikipedia.org/wiki/Apollo_11',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/Aldrin_Apollo_11_original.jpg/520px-Aldrin_Apollo_11_original.jpg',
+        alt: 'Image of astronauts Armstrong and Aldrin on the moon'
+    },
+    {
+        name: 'Malala Yousufzai',
+        role: 'Female Education Activist',
+        story: 'After being shot by the Taliban when she was just 14 years old, Malala Yousufzai continued her work to fight for the right for girls to go to school and became the youngest person ever to win the world-famous Nobel Peace Prize.',
+        link: 'https://en.wikipedia.org/wiki/Malala_Yousafzai',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Shinz%C5%8D_Abe_and_Malala_Yousafzai_%281%29_Cropped.jpg',
+        alt: 'Portrait of Malala Yousufzai wearing a purple hijab'
+    },
+    {
+        name: 'Amelia Earhart',
+        role: 'Aviator and Explorer',
+        story: 'Worldwide sensation Amelia Earhart was the first woman — and only person since Charles Lindbergh — to fly nonstop and alone across the Atlantic. Flying the red Lockheed Vega, she left Harbor Grace, Newfoundland, Canada, and landed 15 hours later near Londonderry, Northern Ireland. ',
+        link: 'https://en.wikipedia.org/wiki/Amelia_Earhart',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Amelia_Earhart_standing_under_nose_of_her_Lockheed_Model_10-E_Electra%2C_small.jpg/500px-Amelia_Earhart_standing_under_nose_of_her_Lockheed_Model_10-E_Electra%2C_small.jpg',
+        alt: 'Portrait of Amelia Earhart standing in front of her plane'
+    },
+    {
+        name: 'Nelson Mandela',
+        role: 'Activist and President',
+        story: 'Nelson Mandela dedicated his life to dismantling racism—and went from being the world’s most famous political prisoner to South Africa’s first Black president. During a lifetime of resistance, imprisonment, and leadership, Nelson Mandela led South Africa out of apartheid and into an era of reconciliation and majority rule.',
+        link: 'https://en.wikipedia.org/wiki/Nelson_Mandela',
+        image: 'https://cdn.britannica.com/93/173193-050-639D3239/Nelson-Mandela-South-African.jpg',
+        alt: 'Portrait of Nelson Mandela resting his chin in his hand'
+    },
+    {
+        name: 'Claudette Colvin',
+        role: 'Civil Rights Activist',
+        story: 'Claudette Colvin is a pioneer of the 1950s civil rights movement and retired nurse aide. She was arrested at the age of 15 in Montgomery, Alabama, for refusing to give up her seat to a white woman on a crowded, segregated bus - nine months before Rosa Parks famous protest.',
+        link: 'https://en.wikipedia.org/wiki/Claudette_Colvin',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Claudette_Colvin.jpg/440px-Claudette_Colvin.jpg',
+        alt: 'Portrait of Claudette Colvin wearing black framed glasses'
+    },
+    {
+        name: 'Muhammad Yunus',
+        role: 'Social Entrepreneur',
+        story: 'Muhammad Yunus was awarded the Nobel Peace Prize for founding the Grameen Bank and pioneering the concepts of microcredit and microfinance. These loans are given to entrepreneurs too poor to qualify for traditional bank loans in order to "create economic and social development from below".',
+        link: 'https://en.wikipedia.org/wiki/Muhammad_Yunus',
+        image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Muhammad_Yunus_%28cropped%29.jpg/440px-Muhammad_Yunus_%28cropped%29.jpg',
+        alt: 'Portrait of Muhammad Yunus wearing a grey shirt and brown vest'
+    },
+    {
+        name: 'Lin Siniang',
+        role: 'Teenage War Leader',
+        story: 'Born into poverty and left to prostitution after her family\'s death, Lin Siniang became the lady of feudal lord, Zhu Changshu. Zhu being impressed with her martial arts skills asked her to train all of his concubines. This led to a fully-fledged female army that later rescued Zhu after being captured by rebel forces.',
+        link: 'https://amazingwomeninhistory.com/lin-siniang-ming-dynasty-warrior/',
+        image: 'https://lsmorganblog.files.wordpress.com/2013/10/cropped-cropped-tumblr_m6pcm7alz31qcvf6m2.jpg',
+        alt: 'Illustration of Lin Siniang wearing several traditional accessories'
+    },
+    {
+        name: 'Juana Belén Gutiérrez de Mendoza',
+        role: 'Anarchist and Feminist Activist',
+        story: 'Juana Belén Gutiérrez de Mendoza founded a newspaper criticising the President of Mexico for not carrying out the needs of the people. Despite being arrested several times she continued her writing and is considered an intellectual contributor to the Mexican Revolution',
+        link: 'https://en.wikipedia.org/wiki/Juana_Bel%C3%A9n_Guti%C3%A9rrez_de_Mendoza',
+        image: 'https://mujeresyrevolucion.files.wordpress.com/2017/03/juana-belecc81n.jpg?w=1086',
+        alt: 'Portrait of Juana Belén Gutiérrez de Mendoza wearing a suit'
+    },
+    {
+        name: 'Stanislav Petrov',
+        role: 'Lieutenant Colonel',
+        story: 'During the height of tension between the USSR and US, Stanislav Petrov correctly identified a false warning indicating the launch of several ballistic missiles from North America. By ignoring instructions to immediately retaliate with a counter-strike, Petrov prevented large-scale nuclear warfare.',
+        link: 'https://en.wikipedia.org/wiki/Stanislav_Petrov',
+        image: 'https://cdn.vox-cdn.com/thumbor/9QKEeUBSuBqifNcyl9oR9Pv3TSM=/0x0:3000x1970/1200x800/filters:focal(1260x745:1740x1225)/cdn.vox-cdn.com/uploads/chorus_image/image/61550601/GettyImages_50803740.0.jpg',
+        alt: 'Portrait of Stanislav Petrov in front of a map, sitting next to a small tv'
+    },
+    {
+        name: 'Ada Blackjack',
+        role: 'Expeditionist and Survivor',
+        story: 'Recruited to join a team of explorers in a speculative attempt to claim Russia\'s Wrangel Island for Canada, Ada Blackjack was abandoned by her crew in extreme freezing conditions as food rations ran out. Blackjack used her traditional Iñupiat skills to survive on the island for eight months.',
+        link: 'https://en.wikipedia.org/wiki/Ada_Blackjack',
+        image: 'https://assets.atlasobscura.com/media/W1siZiIsInVwbG9hZHMvYXNzZXRzLzMyMmU5MzhmZGI4MDYwNmZkNl9FVFBVMDAwNjM3N18xLmpwZyJdLFsicCIsImNvbnZlcnQiLCIiXSxbInAiLCJjb252ZXJ0IiwiLXF1YWxpdHkgODEgLWF1dG8tb3JpZW50Il0sWyJwIiwidGh1bWIiLCI2MDB4PiJdXQ/ETPU0006377_1.jpg',
+        alt: 'Portrait of Ada Blackjack with her young son'
+    }
+]
